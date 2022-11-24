d6db5ace224d42c25dfead17c776802508f7d7eb
Akash <akumar415129.ak@gmail.com>
2022-11-24 20:23:47 +0530

    Products contoller edit

--- backend/controllers/products.controllers.js
+++ backend/controllers/products.controllers.js
@@ -1,42 +1,38 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const product = require("../models/product.models.js");
 // require('dotenv').config()
 
 // const JWT_SECRET = process.env.JWT_SECRET;
 
+const getdata = async (req, res) => {
+  // let { page, sortby, order, category, brand } = req.query;
+  // console.log(category, "   ", brand)
+  //   let data = [];
+  // data = await product.find({$and:[{category:category},{brand:brand}]}).skip(0).limit(9)
+  let data = await product.find({ brand: "Costway" }).limit(2);
+  // console.log(data)
 
-const getdata = async(req, res)=>{
+  // if(sortby && order && category && brand){
+  //     data = await product.find({category:category, brand:brand}).skip(page-1 * 9).limit(9).sort({
+  //         sortby:order=='asc'? 1 : -1
+  //     });
+  // }else if(sortby && order && category){
+  //     data = await product.find({category:category}).skip(page-1 * 9).limit(9).sort({
+  //         sortby:order=='asc'? 1 : -1
+  //     });
+  // }else if(sortby && order && category){
+  //     data = await product.find({category:category}).skip(page-1 * 9).limit(9).sort({
+  //         sortby:order=='asc'? 1 : -1
+  //     });
+  // }
 
-// let { page, sortby, order, category, brand } = req.query;
-// console.log(category, "   ", brand)
-let data = [];
-// data = await product.find({$and:[{category:category},{brand:brand}]}).skip(0).limit(9)
-data = await product.find({brand: "Lifestyle Solutions"}).limit(2);
-// console.log(data)
-
-// if(sortby && order && category && brand){
-//     data = await product.find({category:category, brand:brand}).skip(page-1 * 9).limit(9).sort({
-//         sortby:order=='asc'? 1 : -1
-//     });
-// }else if(sortby && order && category){
-//     data = await product.find({category:category}).skip(page-1 * 9).limit(9).sort({
-//         sortby:order=='asc'? 1 : -1
-//     });
-// }else if(sortby && order && category){
-//     data = await product.find({category:category}).skip(page-1 * 9).limit(9).sort({
-//         sortby:order=='asc'? 1 : -1
-//     });
-// }
-
-
-    res.status(200).send({
-        status: 'success',
-        data: {
-            data
-        }
-    })
-}
-
+  res.status(200).send({
+    status: "success",
+    data: {
+      data,
+    },
+  });
+};
 
 module.exports = getdata;
--- backend/database/connectDB.js
+++ backend/database/connectDB.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connect = () => {
-    return mongoose.connect('mongodb+srv://<username>:<password>@cluster0.lyejynl.mongodb.net/?retryWrites=true&w=majority')
-}
+  return mongoose.connect(
+    "mongodb+srv://<username>:<password>@cluster0.lyejynl.mongodb.net/?retryWrites=true&w=majority"
+  );
+};
 
 // module.exports = connect;
 
-
-
 // const connect = async()=>{
 //     return new Promise((resolve, reject) => {
-        
+
 //         mongoose.connect('mongodb+srv://gauravsapkal:pass123@cluster0.lyejynl.mongodb.net/?retryWrites=true&w=majority', (err) => {
 //             if (err) {
 //                 console.log('Error conencting to DB')
