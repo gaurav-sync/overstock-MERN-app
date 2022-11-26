c872d02ff0224630499a1c0fc1af31e042a350c7
Akash <akumar415129.ak@gmail.com>
2022-11-27 00:59:17 +0530

    Product  Page Final Commit Working Fine

--- src/Pages/Product_page/sortDropdown.jsx
+++ src/Pages/Product_page/sortDropdown.jsx
@@ -11,173 +11,208 @@ import {
   getLoading,
 } from "../../redux/action";
 
-
-
-export default function SortDropdown({ page, sortby, setSort, filterDataCat, filterDataBrand }) {
+export default function SortDropdown({
+  page,
+  sortby,
+  setSort,
+  filterDataCat,
+  filterDataBrand,
+}) {
   const state = useSelector((state) => state);
   const { loading, error, furnitures } = state;
   const [searchParams, setSearchParams] = useSearchParams();
   const [order, setOrder] = useState("");
   const dispatch = useDispatch();
-  const [category, setCategory] = useState(searchParams.getAll("category") || []);
+  const [category, setCategory] = useState(
+    searchParams.getAll("category") || []
+  );
   const [brands, setBrands] = useState(searchParams.getAll("brands") || []);
 
-  // useEffect(() => {
-  //   if (sortby !="") {
-  //     dispatch(getLoading());
-  //     getFurnitureDataWithParams({ _sortby: "price", _order: sortby },page).then(
-  //       (res) => {
-  //         dispatch(getFurniture(res.data));
-  //       }
-  //     );
-  //   }
-  //   else {
-  //     dispatch(getLoading());
-  //     dispatch(getFurnitureData());
-  //   }
-  // }, [sortby,page,filterDataBrand,filterDataCat]);
-
   useEffect(() => {
-    dispatch(getLoading())
+    dispatch(getLoading());
     if (filterDataBrand.length && filterDataCat.length) {
       if (sortby != "") {
-        getFurnitureDataFilter(filterDataCat, filterDataBrand, "price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          filterDataBrand,
+          "price",
+          sortby,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       } else {
-        getFurnitureDataFilter(filterDataCat, filterDataBrand, undefined, undefined, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          filterDataBrand,
+          undefined,
+          undefined,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       }
-    }
-    else if (filterDataBrand.length == 0)
+    } else if (filterDataBrand.length == 0)
       if (sortby != "") {
-        getFurnitureDataFilter(filterDataCat, undefined, "price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          undefined,
+          "price",
+          sortby,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       } else {
-        getFurnitureDataFilter(filterDataCat, undefined, undefined, undefined, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          undefined,
+          undefined,
+          undefined,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       }
     else if (filterDataCat.length == 0) {
       if (sortby != "") {
-        getFurnitureDataFilter(undefined, filterDataBrand, "price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          undefined,
+          filterDataBrand,
+          "price",
+          sortby,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       } else {
-        getFurnitureDataFilter(undefined, filterDataBrand, undefined, undefined, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          undefined,
+          filterDataBrand,
+          undefined,
+          undefined,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       }
     } else {
-      getFurnitureDataFilter(undefined, undefined, sortby, page).then((res) => dispatch(getFurniture(res.data)))
+      getFurnitureDataFilter(undefined, undefined, sortby, page).then((res) =>
+        dispatch(getFurniture(res.data))
+      );
     }
-  }, [filterDataCat, filterDataBrand, page, sortby])
+  }, [filterDataCat, filterDataBrand, page, sortby]);
 
   useEffect(() => {
-    dispatch(getLoading())
+    dispatch(getLoading());
     if (filterDataBrand.length && filterDataCat.length) {
       if (sortby != "") {
-        getFurnitureDataFilter(filterDataCat, filterDataBrand, "price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          filterDataBrand,
+          "price",
+          sortby,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       } else {
-        getFurnitureDataFilter(filterDataCat, filterDataBrand, undefined, undefined, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          filterDataBrand,
+          undefined,
+          undefined,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       }
-    }else if(!filterDataBrand.length && !filterDataCat.length) {
-      getFurnitureDataFilter(undefined, undefined,"price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
-    }
-    else if (filterDataBrand.length == 0)
+    } else if (!filterDataBrand.length && !filterDataCat.length) {
+      getFurnitureDataFilter(undefined, undefined, "price", sortby, page).then(
+        (res) => dispatch(getFurniture(res.data))
+      );
+    } else if (filterDataBrand.length == 0)
       if (sortby != "") {
-        getFurnitureDataFilter(filterDataCat, undefined, "price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          undefined,
+          "price",
+          sortby,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       } else {
-        getFurnitureDataFilter(filterDataCat, undefined, undefined, undefined, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          filterDataCat,
+          undefined,
+          undefined,
+          undefined,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       }
     else if (filterDataCat.length == 0) {
       if (sortby != "") {
-        getFurnitureDataFilter(undefined, filterDataBrand, "price", sortby, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          undefined,
+          filterDataBrand,
+          "price",
+          sortby,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       } else {
-        getFurnitureDataFilter(undefined, filterDataBrand, undefined, undefined, page).then((res) => dispatch(getFurniture(res.data)))
+        getFurnitureDataFilter(
+          undefined,
+          filterDataBrand,
+          undefined,
+          undefined,
+          page
+        ).then((res) => dispatch(getFurniture(res.data)));
       }
-    } 
-  }, [])
+    }
+  }, []);
   useEffect(() => {
     const obj = { page };
     if (filterDataBrand.length && filterDataCat.length) {
       if (sortby != "") {
         obj.order = sortby;
-        obj.sortby = "price"
+        obj.sortby = "price";
       }
       obj.category = filterDataCat;
-      obj.brands = filterDataBrand
-    }
-    else if (filterDataBrand.length == 0) {
+      obj.brands = filterDataBrand;
+    } else if (filterDataBrand.length == 0) {
       if (sortby != "") {
         obj.order = sortby;
-        obj.sortby = "price"
+        obj.sortby = "price";
       }
       obj.category = filterDataCat;
-    }
-    else if (filterDataCat.length == 0) {
+    } else if (filterDataCat.length == 0) {
       if (sortby != "") {
         obj.order = sortby;
-        obj.sortby = "price"
+        obj.sortby = "price";
       }
-      obj.brands = filterDataBrand
+      obj.brands = filterDataBrand;
     } else {
       if (sortby != "") {
         obj.order = sortby;
-        obj.sortby = "price"
+        obj.sortby = "price";
       }
     }
 
     setSearchParams(obj);
-  }, [filterDataCat, filterDataBrand, page, sortby])
+  }, [filterDataCat, filterDataBrand, page, sortby]);
 
   useEffect(() => {
-    if (sortby !="") {
+    if (sortby != "") {
       dispatch(getLoading());
-      getFurnitureDataWithParams({ sortby: "price", order: sortby },page).then(
+      getFurnitureDataWithParams({ sortby: "price", order: sortby }, page).then(
         (res) => {
           dispatch(getFurniture(res.data));
         }
       );
-    }
-    else {
+    } else {
       dispatch(getLoading());
-      dispatch(getFurnitureData());   
+      dispatch(getFurnitureData());
     }
   }, []);
-
-
-  // useEffect(() => {
-  //   dispatch(getLoading())
-  //   if (brands.length && category.length) {
-  //     if (sortby != "") {
-  //       getFurnitureDataFilter(category, brands, "price", sortby).then((res) => dispatch(getFurniture(res.data)))
-  //     } else {
-  //       getFurnitureDataFilter(category, brands, undefined, undefined).then((res) => dispatch(getFurniture(res.data)))
-  //     }
-  //   }
-  //   else if (brands.length == 0)
-  //     if (sortby != "") {
-  //       getFurnitureDataFilter(category, undefined, "price", sortby).then((res) => dispatch(getFurniture(res.data)))
-  //     } else {
-  //       getFurnitureDataFilter(category, undefined, undefined, undefined).then((res) => dispatch(getFurniture(res.data)))
-  //     }
-  //   else if (category.length == 0) {
-  //     if (sortby != "") {
-  //       getFurnitureDataFilter(undefined, brands, "price", sortby).then((res) => dispatch(getFurniture(res.data)))
-  //     } else {
-  //       getFurnitureDataFilter(undefined, brands, undefined, undefined).then((res) => dispatch(getFurniture(res.data)))
-  //     }
-  //   } else {
-  //     dispatch(getFurnitureData());
-  //   }
-  // }, [category, brands])
 
   useEffect(() => {
     const obj = {
       page,
-      order:sortby,
-      "sortby": "price"
-    }
+      order: sortby,
+      sortby: "price",
+    };
     if (brands.length > 0) {
       obj.brands = brands;
     }
     if (category.length > 0) {
       obj.category = category;
     }
-    if (sortby != "") setSearchParams(obj)
+    if (sortby != "") setSearchParams(obj);
     else setSearchParams({ page });
-  }, [sortby])
+  }, [sortby]);
 
   const handleSort = (e) => {
     setSort(e.target.value);
@@ -185,16 +220,19 @@ export default function SortDropdown({ page, sortby, setSort, filterDataCat, fil
   return (
     <HStack justifyContent="flex-end" m={3}>
       <Heading fontSize={22} fontWeight="normal" marginRight={2}>
-        sortby By:{" "}
+        Sort By:{" "}
       </Heading>
       <Select w={200} onChange={handleSort} className="selector">
-        <option value="" selected={sortby=="" ? true : false}>Select option</option>
-        <option value="desc" selected={sortby=="desc" ? true : false}>Price : High to Low</option>
-        <option value="asc" selected={sortby=="asc" ? true : false}>Price : Low to High</option>
+        <option value="" selected={sortby == "" ? true : false}>
+          Select option
+        </option>
+        <option value="desc" selected={sortby == "desc" ? true : false}>
+          Price : High to Low
+        </option>
+        <option value="asc" selected={sortby == "asc" ? true : false}>
+          Price : Low to High
+        </option>
       </Select>
     </HStack>
   );
 }
-
-
-
